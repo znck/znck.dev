6328d8e89b59b14cb9d0fef28aeca7b9dd8dd94e
znck <znck@users.noreply.github.com>
2026-01-06 04:44:27 +0000

    deploy: znck/code@aa94fa11e16524e600bdd74551a0df77fc241eb2

--- _assets/MIT-C4znubwh.js
+++ _assets/MIT-C4znubwh.js
@@ -1,4 +1,4 @@
-import{_ as i,f as n,o as s,e,F as a}from"./index-DNcmfDFm.js";const o={type:"page",title:"",description:`Permission is hereby granted, free of charge, to any person obtaining a copy
+import{_ as i,f as n,o as s,e,F as a}from"./index-COOEJDXV.js";const o={type:"page",title:"",description:`Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
